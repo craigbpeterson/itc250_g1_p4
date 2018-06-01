5d14fc3795d93fea61043bafbdfd60e04105efa6
Craig Peterson <craig.b.peterson@gmail.com>
2018-06-01 15:12:13 -0700

    fixed titleTag, metaDescription, and metaKeywords
    $config->titleTag, $config->metaDescription, $config->metaKeywords referenced "Surveys" instead of "RSS News Feeds" since I based this file on the surveys/index.php file

--- news/index.php
+++ news/index.php
@@ -22,11 +22,11 @@ date_format(f.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from " . PREFIX . "rss
 ";
 
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
-$config->titleTag = 'Surveys made with love & PHP in Seattle';
+$config->titleTag = 'RSS News Feeds made with love & PHP in Seattle';
 
 #Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-$config->metaDescription = 'Seattle Central\'s ITC250 Class Surveys are made with pure PHP! ' . $config->metaDescription;
-$config->metaKeywords = 'Surveys,PHP,Fun,'. $config->metaKeywords;
+$config->metaDescription = 'Seattle Central\'s ITC250 RSS News Feeds are made with pure PHP! ' . $config->metaDescription;
+$config->metaKeywords = 'RSS News Feeds,PHP,Fun,'. $config->metaKeywords;
 
 //adds font awesome icons for arrows on pager
 $config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
